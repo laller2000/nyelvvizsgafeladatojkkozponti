df2143e73fb2a9efae9dbaf0eae3bf873beaed85
laller2000 <69633175+laller2000@users.noreply.github.com>
2021-04-17 17:40:53 +0200

    1.0
    1.0

---
+++ Nyelvvizsga/Nyelvvizsga/Nyelvvizsga.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Nyelvvizsga
+{
+    class Nyelvvizsga
+    {
+        static List<Sikeresvizsga> sikeres = new List<Sikeresvizsga>();
+        static List<Sikertelenvizsga> sikertelen = new List<Sikertelenvizsga>();
+        static int evszamlalo=0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.feladat:sikertelen és sikeres csv beolvasása");
+            Beolvas1("sikeres.csv");
+            Beolvas2("sikertelen.csv");
+            Console.WriteLine("\n2.feladat:A legnépszerűbb nyelvek:");
+            int osszessikeres = 0;
+            int osszessikertelen = 0;
+            int osszeg = 0;
+            foreach (Sikeresvizsga item in sikeres)
+            {
+                osszessikeres += item.Ev9 + item.Ev10 + item.Ev11+item.Ev12+item.Ev13+item.Ev14+item.Ev15+item.Ev16+item.Ev17+item.Ev18;
+                Console.WriteLine(item.Nyelv+osszessikeres);
+            }
+            foreach (Sikertelenvizsga item in sikertelen)
+            {
+                osszessikertelen += item.Ev9 + item.Ev10+item.Ev11+item.Ev12+item.Ev13+item.Ev14+item.Ev15+item.Ev16+item.Ev17+item.Ev918;
+                Console.WriteLine("\n"+item.Nyelv+osszessikertelen);
+            }
+            osszeg = osszessikeres + osszessikertelen;
+            Dictionary<string, int> nyelveksikeres = new Dictionary<string, int>();
+            foreach (Sikeresvizsga item in sikeres)
+            {
+                if (nyelveksikeres.ContainsKey(item.Nyelv))
+                {
+                   
+                }
+                else
+                {
+                    nyelveksikeres.Add(item.Nyelv, osszessikeres);
+                }
+            }
+            Dictionary<string,int> nyelveksikertelen = new Dictionary<string, int>();
+            foreach (Sikertelenvizsga item in sikertelen)
+            {
+                if (nyelveksikertelen.ContainsKey(item.Nyelv))
+                {
+
+                }
+                else
+                {
+                    nyelveksikertelen.Add(item.Nyelv, osszessikertelen);
+                }
+            }
+            var legnepszerubb=nyelveksikertelen.OrderByDescending(a => a.Key).Take(3);
+            var legnepszerubb2 = nyelveksikeres.OrderByDescending(b => b.Key).Take(3);
+            foreach (var item in legnepszerubb)
+            {
+                Console.WriteLine(item.Key);
+            }
+            foreach (var item in legnepszerubb2)
+            {
+                Console.WriteLine(item.Key);
+            }
+            Console.WriteLine("\n3.feladat:");
+            int evszam;
+            do
+            {
+                Console.Write("Vizsgálandó év:");
+                 evszam = int.Parse(Console.ReadLine());
+            } while (evszam<2009 && evszam>=2017);
+            Console.WriteLine("\n4.feladat:");
+            double maxarany = 0;
+            double aranysikeres = 0;
+            double aranysikertelen = 0;
+            foreach (Sikeresvizsga item in sikeres)
+            {
+                aranysikeres = osszeg + osszessikeres / evszamlalo;
+                Console.WriteLine(item.Nyelv+aranysikeres);
+            }
+            foreach (Sikertelenvizsga item in sikertelen)
+            {
+                aranysikertelen = osszessikertelen + osszeg / evszamlalo;
+                if (evszam==2009 || evszam==2010 || evszam==2011 || evszam==2012 || evszam==2013 || evszam==2014 || evszam==2015 || evszam==2016 || evszam==2017 || evszam==2018 && aranysikertelen>maxarany)
+                {
+                    maxarany = aranysikertelen;
+                    Console.WriteLine($"{evszam}-ben{item.Nyelv} nyelvből a sikertelen vizsgázók aránya:{maxarany}%");
+                }
+                
+            }
+            
+            Console.WriteLine("\n5.feladat:");
+            bool vannyelv = false;
+            foreach (Sikertelenvizsga item in sikertelen)
+            {
+                if (evszam==2009 || evszam==2010 || evszam==2011 || evszam==2012 || evszam==2013 || evszam==2014 || evszam==2015 || evszam==2016 || evszam==2017 || evszam==2018)
+                {
+                    if (item.Ev9==0 ||  item.Ev10==0 || item.Ev11==0 || item.Ev12==0 || item.Ev13==0 || item.Ev14==0 || item.Ev15==0 || item.Ev16==0 || item.Ev17==0 || item.Ev918==0)
+                    {
+                        vannyelv = true;
+                        Console.WriteLine(item.Nyelv);
+                        break;
+                    }
+                }
+            }
+            if (!vannyelv)
+            {
+                Console.WriteLine("Minden nyelvből volt vizsgázó!");
+            }
+            Console.WriteLine("\n6.feladat:");
+            Dictionary<string, double> SikeresAranyszotar = new Dictionary<string, double>();
+            foreach (Sikeresvizsga item in sikeres)
+            {
+                if (SikeresAranyszotar.ContainsKey(item.Nyelv))
+                {
+
+                }
+                else
+                {
+                    SikeresAranyszotar.Add(item.Nyelv, aranysikeres);
+                }
+
+            }
+            foreach (var item in SikeresAranyszotar)
+            {
+                Console.WriteLine(item.Key,item.Value);
+            }
+            using (StreamWriter iras=new StreamWriter("osszesites.csv",false,Encoding.Default))
+            {
+                foreach (Sikeresvizsga item in sikeres)
+                {
+                    iras.WriteLine($"{item.Nyelv};{osszessikeres};{aranysikeres}");
+                }
+                iras.Close();
+            }
+            Console.WriteLine("\nProgram Vége!");
+            Console.ReadLine();
+        }
+        static void Beolvas1(string filenev)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filenev,Encoding.Default))
+                {
+                    string fejlec = sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        evszamlalo++;
+                        sikeres.Add(new Sikeresvizsga(sr.ReadLine()));
+                    }
+                    sr.Close();
+                }
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            
+        }
+        static void Beolvas2(string filenev)
+        {
+            try
+            {
+                using (StreamReader sr2=new StreamReader(filenev,Encoding.Default))
+                {
+                    string fejlec = sr2.ReadLine();
+                    while (!sr2.EndOfStream)
+                    {
+                        evszamlalo++;
+                        sikertelen.Add(new Sikertelenvizsga(sr2.ReadLine()));
+                    }
+                    sr2.Close();
+                }
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine(ioex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Nyelvvizsga/Nyelvvizsga/Sikertelenvizsga.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvvizsga
+{
+    class Sikertelenvizsga
+    {
+        //nyelv;2009;2010;2011;2012;2013;2014;2015;2016;2017;2018
+        private string nyelv;
+        private int ev9;
+        private int ev10;
+        private int ev11;
+        private int ev12;
+        private int ev13;
+        private int ev14;
+        private int ev15;
+        private int ev16;
+        private int ev17;
+        private int ev918;
+
+        public string Nyelv { get => nyelv; set => nyelv = value; }
+        public int Ev9 { get => ev9; set => ev9 = value; }
+        public int Ev10 { get => ev10; set => ev10 = value; }
+        public int Ev11 { get => ev11; set => ev11 = value; }
+        public int Ev12 { get => ev12; set => ev12 = value; }
+        public int Ev13 { get => ev13; set => ev13 = value; }
+        public int Ev14 { get => ev14; set => ev14 = value; }
+        public int Ev15 { get => ev15; set => ev15 = value; }
+        public int Ev16 { get => ev16; set => ev16 = value; }
+        public int Ev17 { get => ev17; set => ev17 = value; }
+        public int Ev918 { get => ev918; set => ev918 = value; }
+
+        public Sikertelenvizsga(string nyelv, int ev9, int ev10, int ev11, int ev12, int ev13, int ev14, int ev15, int ev16, int ev17, int ev918)
+        {
+            Nyelv = nyelv;
+            Ev9 = ev9;
+            Ev10 = ev10;
+            Ev11 = ev11;
+            Ev12 = ev12;
+            Ev13 = ev13;
+            Ev14 = ev14;
+            Ev15 = ev15;
+            Ev16 = ev16;
+            Ev17 = ev17;
+            Ev918 = ev918;
+        }
+        public Sikertelenvizsga(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Nyelv = adatok[0];
+            Ev9 = int.Parse(adatok[1]);
+            Ev10 = int.Parse(adatok[2]);
+            Ev11 = int.Parse(adatok[3]);
+            Ev12 = int.Parse(adatok[4]);
+            Ev13 = int.Parse(adatok[5]);
+            Ev14 = int.Parse(adatok[6]);
+            Ev15 = int.Parse(adatok[7]);
+            Ev16 = int.Parse(adatok[8]);
+            Ev17 = int.Parse(adatok[9]);
+            Ev918 = int.Parse(adatok[10]);
+        }
+    }
+}
